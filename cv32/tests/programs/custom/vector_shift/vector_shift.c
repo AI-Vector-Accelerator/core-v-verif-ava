197c7ec056e7628018b9d5451aceab1f3b7b6c91
P.Alexander <pa4g17@soton.ac.uk>
2020-12-04 18:38:37 +0000

    Expand vector_shift testbench for m1/m2/m4 register groupings

--- cv32/tests/programs/custom/vector_shift/vector_shift.c
+++ cv32/tests/programs/custom/vector_shift/vector_shift.c
@@ -3,9 +3,9 @@
 #include <stdint.h>
 
 
-int shift_left_test_e8(uint8_t vlen) 
+int shift_left_test_e8_m1(uint8_t vlen) 
 {
-    printf("Starting Left Shift e8 Test...");
+    printf("Starting Left Shift e8 m1 Test...");
     uint8_t output;
     asm volatile (
         "vsetvli t2, %1, e8\n\t"
@@ -24,10 +24,64 @@ int shift_left_test_e8(uint8_t vlen)
     
 }
 
-
-int shift_right_test_e8(uint8_t vlen) 
+int shift_left_test_e8_m2(uint8_t vlen) 
 {
-    printf("Starting Right Shift e8 Test...");
+    printf("Starting Left Shift e8 m2 Test...");
+    uint8_t output0, output1;
+    asm volatile (
+        "vsetvli t2, %1, e8\n\t"
+        "vmv.v.i v2, 2\n\t"
+        "vmv.v.i v3, 4\n\t"
+        "vsetvli t2, %1, e8, m2\n\t"
+        "vsll.vi v4, v2, 1\n\t"
+        "vsetvli t2, %1, e8\n\t"
+        "vmv.x.s %0, v4\n\t"
+        "vmv.x.s %1, v5"
+        : "=r" (output0), "=r" (output1)
+        : "r" (vlen)
+    );
+
+    printf("Test Complete! Output: v4: %d\tv5: %d\n", output0, output1);
+    if(output0 != 4 || output1 != 8)
+        return(1);
+    else
+        return(0);
+    
+}
+
+int shift_left_test_e8_m4(uint8_t vlen) 
+{
+    printf("Starting Left Shift e8 m4 Test...");
+    uint8_t output0, output1, output2, output3;
+    asm volatile (
+        "vsetvli t2, %1, e8\n\t"
+        "vmv.v.i v4, 1\n\t"
+        "vmv.v.i v5, 2\n\t"
+        "vmv.v.i v6, 4\n\t"
+        "vmv.v.i v7, 8\n\t"
+        "vsetvli t2, %1, e8, m4\n\t"
+        "vsll.vi v8, v4, 1\n\t"
+        "vsetvli t2, %1, e8\n\t"
+        "vmv.x.s %0, v8\n\t"
+        "vmv.x.s %1, v9\n\t"
+        "vmv.x.s %2, v10\n\t"
+        "vmv.x.s %3, v11\n\t"
+        : "=r" (output0), "=r" (output1), "=r" (output2), "=r" (output3)
+        : "r" (vlen)
+    );
+
+    printf("Test Complete! Output: v8: %d\tv9: %d\tv10: %d\tv11: %d\n", output0, output1, output2, output3);
+    if(output0 != 2 || output1 != 4 || output2 != 8 || output3 != 16)
+        return(1);
+    else
+        return(0);
+}
+
+
+
+int shift_right_test_e8_m1(uint8_t vlen) 
+{
+    printf("Starting Right Shift e8 m1 Test...");
     uint8_t output;
     asm volatile (
         "vsetvli t2, %1, e8\n\t"
@@ -43,13 +97,70 @@ int shift_right_test_e8(uint8_t vlen)
         return(1);
     else
         return(0);
+    
+}
 
+int shift_right_test_e8_m2(uint8_t vlen) 
+{
+    printf("Starting Right Shift e8 m2 Test...");
+    uint8_t output0, output1;
+    asm volatile (
+        "vsetvli t2, %1, e8\n\t"
+        "vmv.v.i v2, 2\n\t"
+        "vmv.v.i v3, 4\n\t"
+        "vsetvli t2, %1, e8, m2\n\t"
+        "vsrl.vi v4, v2, 1\n\t"
+        "vsetvli t2, %1, e8\n\t"
+        "vmv.x.s %0, v4\n\t"
+        "vmv.x.s %1, v5"
+        : "=r" (output0), "=r" (output1)
+        : "r" (vlen)
+    );
+
+    printf("Test Complete! Output: v4: %d\tv5: %d\n", output0, output1);
+    if(output0 != 1 || output1 != 2)
+        return(1);
+    else
+        return(0);
+    
+}
+
+int shift_right_test_e8_m4(uint8_t vlen) 
+{
+    printf("Starting Right Shift e8 m4 Test...");
+    uint8_t output0, output1, output2, output3;
+    uint8_t len;
+    asm volatile (
+        "vsetvli %4, %1, e8\n\t"
+        "vmv.v.i v4, 1\n\t"
+        "vmv.v.i v5, 2\n\t"
+        "vmv.v.i v6, 4\n\t"
+        "vmv.v.i v7, 8\n\t"
+        "vsetvli  %4, %1, e8, m4\n\t"
+        "vsrl.vi v8, v4, 1\n\t"
+        "vsetvli t2, %1, e8\n\t"
+        "vmv.x.s %0, v8\n\t"
+        "vmv.x.s %1, v9\n\t"
+        "vmv.x.s %2, v10\n\t"
+        "vmv.x.s %3, v11\n\t"
+        : "=r" (output0), "=r" (output1), "=r" (output2), "=r" (output3), "=r" (len)
+        : "r" (vlen)
+    );
+
+    printf("Test Complete! Output: v12: %d\tv13: %d\tv14: %d\tv15: %d\n", output0, output1, output2, output3);
+    printf("Len = %d\n", len);
+    if(output0 != 0 || output1 != 1 || output2 != 2 || output3 != 4)
+        return(1);
+    else
+        return(0);
 }
 
 
-int shift_left_test_e16(uint8_t vlen) 
+
+
+int shift_left_test_e16_m1(uint8_t vlen) 
 {
-    printf("Starting Left Shift e16 Test...");
+    printf("Starting Left Shift e16 m1 Test...");
     uint8_t output;
     asm volatile (
         "vsetvli t2, %1, e16\n\t"
@@ -68,10 +179,64 @@ int shift_left_test_e16(uint8_t vlen)
     
 }
 
-
-int shift_right_test_e16(uint8_t vlen) 
+int shift_left_test_e16_m2(uint8_t vlen) 
 {
-    printf("Starting Right Shift e16 Test...");
+    printf("Starting Left Shift e16 m2 Test...");
+    uint8_t output0, output1;
+    asm volatile (
+        "vsetvli t2, %1, e16\n\t"
+        "vmv.v.i v2, 2\n\t"
+        "vmv.v.i v3, 4\n\t"
+        "vsetvli t2, %1, e16, m2\n\t"
+        "vsll.vi v4, v2, 1\n\t"
+        "vsetvli t2, %1, e16\n\t"
+        "vmv.x.s %0, v4\n\t"
+        "vmv.x.s %1, v5"
+        : "=r" (output0), "=r" (output1)
+        : "r" (vlen)
+    );
+
+    printf("Test Complete! Output: v4: %d\tv5: %d\n", output0, output1);
+    if(output0 != 4 || output1 != 8)
+        return(1);
+    else
+        return(0);
+    
+}
+
+int shift_left_test_e16_m4(uint8_t vlen) 
+{
+    printf("Starting Left Shift e16 m4 Test...");
+    uint8_t output0, output1, output2, output3;
+    asm volatile (
+        "vsetvli t2, %1, e16\n\t"
+        "vmv.v.i v4, 1\n\t"
+        "vmv.v.i v5, 2\n\t"
+        "vmv.v.i v6, 4\n\t"
+        "vmv.v.i v7, 8\n\t"
+        "vsetvli t2, %1, e16, m4\n\t"
+        "vsll.vi v8, v4, 1\n\t"
+        "vsetvli t2, %1, e16\n\t"
+        "vmv.x.s %0, v8\n\t"
+        "vmv.x.s %1, v9\n\t"
+        "vmv.x.s %2, v10\n\t"
+        "vmv.x.s %3, v11\n\t"
+        : "=r" (output0), "=r" (output1), "=r" (output2), "=r" (output3)
+        : "r" (vlen)
+    );
+
+    printf("Test Complete! Output: v8: %d\tv9: %d\tv10: %d\tv11: %d\n", output0, output1, output2, output3);
+    if(output0 != 2 || output1 != 4 || output2 != 8 || output3 != 16)
+        return(1);
+    else
+        return(0);
+}
+
+
+
+int shift_right_test_e16_m1(uint8_t vlen) 
+{
+    printf("Starting Right Shift e16 m1 Test...");
     uint8_t output;
     asm volatile (
         "vsetvli t2, %1, e16\n\t"
@@ -87,13 +252,68 @@ int shift_right_test_e16(uint8_t vlen)
         return(1);
     else
         return(0);
+    
+}
 
+int shift_right_test_e16_m2(uint8_t vlen) 
+{
+    printf("Starting Right Shift e16 m2 Test...");
+    uint8_t output0, output1;
+    asm volatile (
+        "vsetvli t2, %1, e16\n\t"
+        "vmv.v.i v2, 2\n\t"
+        "vmv.v.i v3, 4\n\t"
+        "vsetvli t2, %1, e16, m2\n\t"
+        "vsrl.vi v4, v2, 1\n\t"
+        "vsetvli t2, %1, e16\n\t"
+        "vmv.x.s %0, v4\n\t"
+        "vmv.x.s %1, v5"
+        : "=r" (output0), "=r" (output1)
+        : "r" (vlen)
+    );
+
+    printf("Test Complete! Output: v4: %d\tv5: %d\n", output0, output1);
+    if(output0 != 1 || output1 != 2)
+        return(1);
+    else
+        return(0);
+    
+}
+
+int shift_right_test_e16_m4(uint8_t vlen) 
+{
+    printf("Starting Right Shift e16 m4 Test...");
+    uint8_t output0, output1, output2, output3;
+    uint8_t len;
+    asm volatile (
+        "vsetvli %4, %1, e16\n\t"
+        "vmv.v.i v4, 1\n\t"
+        "vmv.v.i v5, 2\n\t"
+        "vmv.v.i v6, 4\n\t"
+        "vmv.v.i v7, 8\n\t"
+        "vsetvli  %4, %1, e16, m4\n\t"
+        "vsrl.vi v8, v4, 1\n\t"
+        "vsetvli t2, %1, e16\n\t"
+        "vmv.x.s %0, v8\n\t"
+        "vmv.x.s %1, v9\n\t"
+        "vmv.x.s %2, v10\n\t"
+        "vmv.x.s %3, v11\n\t"
+        : "=r" (output0), "=r" (output1), "=r" (output2), "=r" (output3), "=r" (len)
+        : "r" (vlen)
+    );
+
+    printf("Test Complete! Output: v12: %d\tv13: %d\tv14: %d\tv15: %d\n", output0, output1, output2, output3);
+    printf("Len = %d\n", len);
+    if(output0 != 0 || output1 != 1 || output2 != 2 || output3 != 4)
+        return(1);
+    else
+        return(0);
 }
 
 
-int shift_left_test_e32(uint8_t vlen) 
+int shift_left_test_e32_m1(uint8_t vlen) 
 {
-    printf("Starting Left Shift e32 Test...");
+    printf("Starting Left Shift e32 m1 Test...");
     uint8_t output;
     asm volatile (
         "vsetvli t2, %1, e32\n\t"
@@ -112,10 +332,64 @@ int shift_left_test_e32(uint8_t vlen)
     
 }
 
-
-int shift_right_test_e32(uint8_t vlen) 
+int shift_left_test_e32_m2(uint8_t vlen) 
 {
-    printf("Starting Right Shift e32 Test...");
+    printf("Starting Left Shift e32 m2 Test...");
+    uint8_t output0, output1;
+    asm volatile (
+        "vsetvli t2, %1, e32\n\t"
+        "vmv.v.i v2, 2\n\t"
+        "vmv.v.i v3, 4\n\t"
+        "vsetvli t2, %1, e32, m2\n\t"
+        "vsll.vi v4, v2, 1\n\t"
+        "vsetvli t2, %1, e32\n\t"
+        "vmv.x.s %0, v4\n\t"
+        "vmv.x.s %1, v5"
+        : "=r" (output0), "=r" (output1)
+        : "r" (vlen)
+    );
+
+    printf("Test Complete! Output: v4: %d\tv5: %d\n", output0, output1);
+    if(output0 != 4 || output1 != 8)
+        return(1);
+    else
+        return(0);
+    
+}
+
+int shift_left_test_e32_m4(uint8_t vlen) 
+{
+    printf("Starting Left Shift e32 m4 Test...");
+    uint8_t output0, output1, output2, output3;
+    asm volatile (
+        "vsetvli t2, %1, e32\n\t"
+        "vmv.v.i v4, 1\n\t"
+        "vmv.v.i v5, 2\n\t"
+        "vmv.v.i v6, 4\n\t"
+        "vmv.v.i v7, 8\n\t"
+        "vsetvli t2, %1, e32, m4\n\t"
+        "vsll.vi v8, v4, 1\n\t"
+        "vsetvli t2, %1, e32\n\t"
+        "vmv.x.s %0, v8\n\t"
+        "vmv.x.s %1, v9\n\t"
+        "vmv.x.s %2, v10\n\t"
+        "vmv.x.s %3, v11\n\t"
+        : "=r" (output0), "=r" (output1), "=r" (output2), "=r" (output3)
+        : "r" (vlen)
+    );
+
+    printf("Test Complete! Output: v8: %d\tv9: %d\tv10: %d\tv11: %d\n", output0, output1, output2, output3);
+    if(output0 != 2 || output1 != 4 || output2 != 8 || output3 != 16)
+        return(1);
+    else
+        return(0);
+}
+
+
+
+int shift_right_test_e32_m1(uint8_t vlen) 
+{
+    printf("Starting Right Shift e32 m1 Test...");
     uint8_t output;
     asm volatile (
         "vsetvli t2, %1, e32\n\t"
@@ -131,9 +405,63 @@ int shift_right_test_e32(uint8_t vlen)
         return(1);
     else
         return(0);
-
+    
 }
 
+int shift_right_test_e32_m2(uint8_t vlen) 
+{
+    printf("Starting Right Shift e32 m2 Test...");
+    uint8_t output0, output1;
+    asm volatile (
+        "vsetvli t2, %1, e32\n\t"
+        "vmv.v.i v2, 2\n\t"
+        "vmv.v.i v3, 4\n\t"
+        "vsetvli t2, %1, e32, m2\n\t"
+        "vsrl.vi v4, v2, 1\n\t"
+        "vsetvli t2, %1, e32\n\t"
+        "vmv.x.s %0, v4\n\t"
+        "vmv.x.s %1, v5"
+        : "=r" (output0), "=r" (output1)
+        : "r" (vlen)
+    );
+
+    printf("Test Complete! Output: v4: %d\tv5: %d\n", output0, output1);
+    if(output0 != 1 || output1 != 2)
+        return(1);
+    else
+        return(0);
+    
+}
+
+int shift_right_test_e32_m4(uint8_t vlen) 
+{
+    printf("Starting Right Shift e32 m4 Test...");
+    uint8_t output0, output1, output2, output3;
+    uint8_t len;
+    asm volatile (
+        "vsetvli %4, %1, e32\n\t"
+        "vmv.v.i v4, 1\n\t"
+        "vmv.v.i v5, 2\n\t"
+        "vmv.v.i v6, 4\n\t"
+        "vmv.v.i v7, 8\n\t"
+        "vsetvli  %4, %1, e32, m4\n\t"
+        "vsrl.vi v8, v4, 1\n\t"
+        "vsetvli t2, %1, e32\n\t"
+        "vmv.x.s %0, v8\n\t"
+        "vmv.x.s %1, v9\n\t"
+        "vmv.x.s %2, v10\n\t"
+        "vmv.x.s %3, v11\n\t"
+        : "=r" (output0), "=r" (output1), "=r" (output2), "=r" (output3), "=r" (len)
+        : "r" (vlen)
+    );
+
+    printf("Test Complete! Output: v12: %d\tv13: %d\tv14: %d\tv15: %d\n", output0, output1, output2, output3);
+    printf("Len = %d\n", len);
+    if(output0 != 0 || output1 != 1 || output2 != 2 || output3 != 4)
+        return(1);
+    else
+        return(0);
+}
 
 
 int main(int argc, char *argv[])
@@ -147,14 +475,31 @@ int main(int argc, char *argv[])
 
     int errors = 0;
 
-    errors += shift_left_test_e8(4);
-    errors += shift_right_test_e8(4);
+    errors += shift_left_test_e8_m1(64);
+    errors += shift_left_test_e8_m2(64);
+    errors += shift_left_test_e8_m4(64);
+    printf("\n");
+    errors += shift_right_test_e8_m1(64);
+    errors += shift_right_test_e8_m2(64);
+    errors += shift_right_test_e8_m4(64);
+    printf("\n\n");
+    errors += shift_left_test_e16_m1(64);
+    errors += shift_left_test_e16_m2(64);
+    errors += shift_left_test_e16_m4(64);
+    printf("\n");
+    errors += shift_right_test_e16_m1(64);
+    errors += shift_right_test_e16_m2(64);
+    errors += shift_right_test_e16_m4(64);
+    printf("\n\n");
+    errors += shift_left_test_e32_m1(64);
+    errors += shift_left_test_e32_m2(64);
+    errors += shift_left_test_e32_m4(64);
+    printf("\n");
+    errors += shift_right_test_e32_m1(64);
+    errors += shift_right_test_e32_m2(64);
+    errors += shift_right_test_e32_m4(64);
 
-    errors += shift_left_test_e16(4);
-    errors += shift_right_test_e16(4);
 
-    errors += shift_left_test_e32(4);
-    errors += shift_right_test_e32(4);
 
     if(errors > 0)
     {
