9ca28fcf4741547d2589f3f8badd08e159ae07eb
P.Alexander <pa4g17@soton.ac.uk>
2020-12-04 15:30:40 +0000

    Fix vector_shift test program reporting failures incorrectly
    Program tested in spike, works correctly.

--- cv32/tests/programs/custom/vector_shift/vector_shift.c
+++ cv32/tests/programs/custom/vector_shift/vector_shift.c
@@ -16,18 +16,18 @@ int shift_left_test_e8(uint8_t vlen)
         : "r" (vlen)
     );
 
-    printf("Test Complete!\n");
+    printf("Test Complete! Output: %d\n", output);
     if(output != 8)
-        return(0);
-    else
         return(1);
+    else
+        return(0);
     
 }
 
 
 int shift_right_test_e8(uint8_t vlen) 
 {
-    printf("Starting Left Shift e8 Test...");
+    printf("Starting Right Shift e8 Test...");
     uint8_t output;
     asm volatile (
         "vsetvli t2, %1, e8\n\t"
@@ -38,11 +38,11 @@ int shift_right_test_e8(uint8_t vlen)
         : "r" (vlen)
     );
 
-    printf("Test Complete!\n");
+    printf("Test Complete! Output: %d\n", output);
     if(output != 2)
-        return(0);
-    else
         return(1);
+    else
+        return(0);
 
 }
 
@@ -60,18 +60,18 @@ int shift_left_test_e16(uint8_t vlen)
         : "r" (vlen)
     );
 
-    printf("Test Complete!\n");
+    printf("Test Complete! Output: %d\n", output);
     if(output != 8)
-        return(0);
-    else
         return(1);
+    else
+        return(0);
     
 }
 
 
 int shift_right_test_e16(uint8_t vlen) 
 {
-    printf("Starting Left Shift e16 Test...");
+    printf("Starting Right Shift e16 Test...");
     uint8_t output;
     asm volatile (
         "vsetvli t2, %1, e16\n\t"
@@ -82,11 +82,11 @@ int shift_right_test_e16(uint8_t vlen)
         : "r" (vlen)
     );
 
-    printf("Test Complete!\n");
+    printf("Test Complete! Output: %d\n", output);
     if(output != 2)
-        return(0);
-    else
         return(1);
+    else
+        return(0);
 
 }
 
@@ -104,18 +104,18 @@ int shift_left_test_e32(uint8_t vlen)
         : "r" (vlen)
     );
 
-    printf("Test Complete!\n");
+    printf("Test Complete! Output: %d\n", output);
     if(output != 8)
-        return(0);
-    else
         return(1);
+    else
+        return(0);
     
 }
 
 
 int shift_right_test_e32(uint8_t vlen) 
 {
-    printf("Starting Left Shift e32 Test...");
+    printf("Starting Right Shift e32 Test...");
     uint8_t output;
     asm volatile (
         "vsetvli t2, %1, e32\n\t"
@@ -126,11 +126,11 @@ int shift_right_test_e32(uint8_t vlen)
         : "r" (vlen)
     );
 
-    printf("Test Complete!\n");
+    printf("Test Complete! Output: %d\n", output);
     if(output != 2)
-        return(0);
-    else
         return(1);
+    else
+        return(0);
 
 }
 
@@ -139,6 +139,11 @@ int shift_right_test_e32(uint8_t vlen)
 int main(int argc, char *argv[])
 {
     printf("Beginning Vector Shift Test...\n\n");
+    printf("This program writes the number 4 into a vector register\n");
+    printf("then either shifts it left or right.\n");
+    printf("This is performed for 8, 16, and 32 bit wide vector registers.\n");
+    printf("This gives a total of 6 tests.\n\n");
+
 
     int errors = 0;
 
@@ -152,9 +157,12 @@ int main(int argc, char *argv[])
     errors += shift_right_test_e32(4);
 
     if(errors > 0)
+    {
+        printf("\nErrors Detected!\nError Count: %d\n", errors);
         return(EXIT_FAILURE);
+    }
 
-    printf("All Tests Completed!\n");
+    printf("\nAll Tests Completed\nNo Errors Detected!\n");
 
     return(EXIT_SUCCESS);
 }
