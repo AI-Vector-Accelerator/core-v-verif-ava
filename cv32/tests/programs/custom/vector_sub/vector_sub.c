0672b5e42399ba2733b1e0793e848f421c7678de
P.Alexander <pa4g17@soton.ac.uk>
2020-12-14 13:55:48 +0000

    Add tests for all groupings to vector_sub program

--- cv32/tests/programs/custom/vector_sub/vector_sub.c
+++ cv32/tests/programs/custom/vector_sub/vector_sub.c
@@ -27,6 +27,72 @@ int sub_vv_test_e8_m1(void)
     }
 }
 
+int sub_vv_test_e8_m2(void)
+{
+    int result0, result1;
+    asm volatile (
+        "li t1, 7 \n\t"
+        "vsetvli t0, t1, e8, m1 \n\t"
+        "vmv.v.i v2, 15 \n\t"
+        "vmv.v.i v3, 7 \n\t"
+        "vmv.v.i v4, 2 \n\t"
+        "vmv.v.i v5, 4 \n\t"
+        "vsetvli t0, t1, e8, m2 \n\t"
+        "vsub.vv v2, v2, v4 \n\t"
+        "vsetvli t0, t1, e8, m1 \n\t"
+        "vmv.x.s %0, v2\n\t"
+        "vmv.x.s %1, v3\n\t"
+        : "=r" (result0), "=r" (result1)
+    );
+
+    if(result0 != 13 || result1 != 3)
+    {
+        printf("Fail: %d %d\n", result0, result1);
+        return(1);
+    }
+    else
+    {
+        printf("Pass: %d %d\n", result0, result1);
+        return(0);
+    }
+}
+
+int sub_vv_test_e8_m4(void)
+{
+    int result0, result1, result2, result3;
+    asm volatile (
+        "li t1, 15 \n\t"
+        "vsetvli t0, t1, e8, m1 \n\t"
+        "vmv.v.i v4, 15 \n\t"
+        "vmv.v.i v5, 7 \n\t"
+        "vmv.v.i v6, 2 \n\t"
+        "vmv.v.i v7, 4 \n\t"
+        "vmv.v.i v8, 15 \n\t"
+        "vmv.v.i v9, 13 \n\t"
+        "vmv.v.i v10, 5 \n\t"
+        "vmv.v.i v11, 8 \n\t"
+        "vsetvli t0, t1, e8, m4 \n\t"
+        "vsub.vv v4, v4, v8 \n\t"
+        "vsetvli t0, t1, e8, m1 \n\t"
+        "vmv.x.s %0, v4 \n\t"
+        "vmv.x.s %1, v5 \n\t"
+        "vmv.x.s %2, v6 \n\t"
+        "vmv.x.s %3, v7 \n\t"
+        : "=r" (result0), "=r" (result1), "=r" (result2), "=r" (result3)
+    );
+
+    if(result0 != 0 || result1 != -6 || result2 != -3 || result3 != -4)
+    {
+        printf("Fail: %d %d %d %d\n", result0, result1, result2, result3);
+        return(1);
+    }
+    else
+    {
+        printf("Pass: %d %d %d %d\n", result0, result1, result2, result3);
+        return(0);
+    }
+}
+
 
 int sub_vv_test_e16_m1(void)
 {
@@ -53,6 +119,72 @@ int sub_vv_test_e16_m1(void)
     }
 }
 
+int sub_vv_test_e16_m2(void)
+{
+    int result0, result1;
+    asm volatile (
+        "li t1, 7 \n\t"
+        "vsetvli t0, t1, e16, m1 \n\t"
+        "vmv.v.i v2, 15 \n\t"
+        "vmv.v.i v3, 7 \n\t"
+        "vmv.v.i v4, 2 \n\t"
+        "vmv.v.i v5, 4 \n\t"
+        "vsetvli t0, t1, e16, m2 \n\t"
+        "vsub.vv v2, v2, v4 \n\t"
+        "vsetvli t0, t1, e16, m1 \n\t"
+        "vmv.x.s %0, v2\n\t"
+        "vmv.x.s %1, v3\n\t"
+        : "=r" (result0), "=r" (result1)
+    );
+
+    if(result0 != 13 || result1 != 3)
+    {
+        printf("Fail: %d %d\n", result0, result1);
+        return(1);
+    }
+    else
+    {
+        printf("Pass: %d %d\n", result0, result1);
+        return(0);
+    }
+}
+
+int sub_vv_test_e16_m4(void)
+{
+    int result0, result1, result2, result3;
+    asm volatile (
+        "li t1, 15 \n\t"
+        "vsetvli t0, t1, e16, m1 \n\t"
+        "vmv.v.i v4, 15 \n\t"
+        "vmv.v.i v5, 7 \n\t"
+        "vmv.v.i v6, 2 \n\t"
+        "vmv.v.i v7, 4 \n\t"
+        "vmv.v.i v8, 15 \n\t"
+        "vmv.v.i v9, 13 \n\t"
+        "vmv.v.i v10, 5 \n\t"
+        "vmv.v.i v11, 8 \n\t"
+        "vsetvli t0, t1, e16, m4 \n\t"
+        "vsub.vv v4, v4, v8 \n\t"
+        "vsetvli t0, t1, e16, m1 \n\t"
+        "vmv.x.s %0, v4 \n\t"
+        "vmv.x.s %1, v5 \n\t"
+        "vmv.x.s %2, v6 \n\t"
+        "vmv.x.s %3, v7 \n\t"
+        : "=r" (result0), "=r" (result1), "=r" (result2), "=r" (result3)
+    );
+
+    if(result0 != 0 || result1 != -6 || result2 != -3 || result3 != -4)
+    {
+        printf("Fail: %d %d %d %d\n", result0, result1, result2, result3);
+        return(1);
+    }
+    else
+    {
+        printf("Pass: %d %d %d %d\n", result0, result1, result2, result3);
+        return(0);
+    }
+}
+
 
 int sub_vv_test_e32_m1(void)
 {
@@ -75,6 +207,72 @@ int sub_vv_test_e32_m1(void)
     else
     {
         printf("Pass: %d\n", result);
+        return(0);
+    }
+}
+
+int sub_vv_test_e32_m2(void)
+{
+    int result0, result1;
+    asm volatile (
+        "li t1, 7 \n\t"
+        "vsetvli t0, t1, e32, m1 \n\t"
+        "vmv.v.i v2, 15 \n\t"
+        "vmv.v.i v3, 7 \n\t"
+        "vmv.v.i v4, 2 \n\t"
+        "vmv.v.i v5, 4 \n\t"
+        "vsetvli t0, t1, e32, m2 \n\t"
+        "vsub.vv v2, v2, v4 \n\t"
+        "vsetvli t0, t1, e32, m1 \n\t"
+        "vmv.x.s %0, v2\n\t"
+        "vmv.x.s %1, v3\n\t"
+        : "=r" (result0), "=r" (result1)
+    );
+
+    if(result0 != 13 || result1 != 3)
+    {
+        printf("Fail: %d %d\n", result0, result1);
+        return(1);
+    }
+    else
+    {
+        printf("Pass: %d %d\n", result0, result1);
+        return(0);
+    }
+}
+
+int sub_vv_test_e32_m4(void)
+{
+    int result0, result1, result2, result3;
+    asm volatile (
+        "li t1, 15 \n\t"
+        "vsetvli t0, t1, e32, m1 \n\t"
+        "vmv.v.i v4, 15 \n\t"
+        "vmv.v.i v5, 7 \n\t"
+        "vmv.v.i v6, 2 \n\t"
+        "vmv.v.i v7, 4 \n\t"
+        "vmv.v.i v8, 15 \n\t"
+        "vmv.v.i v9, 13 \n\t"
+        "vmv.v.i v10, 5 \n\t"
+        "vmv.v.i v11, 8 \n\t"
+        "vsetvli t0, t1, e32, m4 \n\t"
+        "vsub.vv v4, v4, v8 \n\t"
+        "vsetvli t0, t1, e32, m1 \n\t"
+        "vmv.x.s %0, v4 \n\t"
+        "vmv.x.s %1, v5 \n\t"
+        "vmv.x.s %2, v6 \n\t"
+        "vmv.x.s %3, v7 \n\t"
+        : "=r" (result0), "=r" (result1), "=r" (result2), "=r" (result3)
+    );
+
+    if(result0 != 0 || result1 != -6 || result2 != -3 || result3 != -4)
+    {
+        printf("Fail: %d %d %d %d\n", result0, result1, result2, result3);
+        return(1);
+    }
+    else
+    {
+        printf("Pass: %d %d %d %d\n", result0, result1, result2, result3);
         return(0);
     }
 }
@@ -107,6 +305,70 @@ int sub_vx_test_e8_m1(void)
     }
 }
 
+int  sub_vx_test_e8_m2(void)
+{  
+    int subend = 8;
+    int result0, result1;
+    asm volatile (
+        "li t1, 64 \n\t"
+        "vsetvli t0, t1, e8, m1 \n\t"
+        "vmv.v.i v2, 3 \n\t"
+        "vmv.v.i v3, 12 \n\t"
+        "vsetvli t0, t1, e8, m2\n\t"
+        "vsub.vx v6, v2, %2 \n\t"
+        "vsetvli t0, t1, e8, m1 \n\t"
+        "vmv.x.s %0, v6\n\t"
+        "vmv.x.s %1, v7\n\t"
+        : "=r" (result0), "=r" (result1) 
+        : "r"  (subend)
+    );
+
+    if(result0 != -5 || result1 != 4)
+    {
+        printf("Fail: %d %d\n", result0, result1);
+        return(1);
+    }
+    else
+    {
+        printf("Pass: %d %d\n", result0, result1);
+        return(0);
+    }
+}
+
+int sub_vx_test_e8_m4(void)
+{  
+    int addend = 8;
+    int result0, result1, result2, result3;
+    asm volatile (
+        "li t1, 64 \n\t"
+        "vsetvli t0, t1, e8, m1 \n\t"
+        "vmv.v.i v4, 8 \n\t"
+        "vmv.v.i v5, 1 \n\t"
+        "vmv.v.i v6, 3 \n\t"
+        "vmv.v.i v7, 15 \n\t"
+        "vsetvli t0, t1, e8, m4\n\t"
+        "vsub.vx v8, v4, %4  \n\t"
+        "vsetvli t0, t1, e8, m1 \n\t"
+        "vmv.x.s %0, v8\n\t"
+        "vmv.x.s %1, v9\n\t"
+        "vmv.x.s %2, v10\n\t"
+        "vmv.x.s %3, v11\n\t"
+        : "=r" (result0), "=r" (result1), "=r" (result2), "=r" (result3)
+        : "r"  (addend)
+    );
+
+    if(result0 != 0 || result1 != -7 || result2 != -5 || result3 != 7)
+    {
+        printf("Fail: %d %d %d %d\n", result0, result1, result2, result3);
+        return(1);
+    }
+    else
+    {
+        printf("Pass: %d %d %d %d\n", result0, result1, result2, result3);
+        return(0);
+    }
+}
+
 int sub_vx_test_e16_m1(void)
 {  
     int subend = 23;
@@ -129,6 +391,70 @@ int sub_vx_test_e16_m1(void)
     else
     {
         printf("Pass: %d\n", result);
+        return(0);
+    }
+}
+
+int  sub_vx_test_e16_m2(void)
+{  
+    int subend = 8;
+    int result0, result1;
+    asm volatile (
+        "li t1, 64 \n\t"
+        "vsetvli t0, t1, e16, m1 \n\t"
+        "vmv.v.i v2, 3 \n\t"
+        "vmv.v.i v3, 12 \n\t"
+        "vsetvli t0, t1, e16, m2\n\t"
+        "vsub.vx v6, v2, %2 \n\t"
+        "vsetvli t0, t1, e16, m1 \n\t"
+        "vmv.x.s %0, v6\n\t"
+        "vmv.x.s %1, v7\n\t"
+        : "=r" (result0), "=r" (result1) 
+        : "r"  (subend)
+    );
+
+    if(result0 != -5 || result1 != 4)
+    {
+        printf("Fail: %d %d\n", result0, result1);
+        return(1);
+    }
+    else
+    {
+        printf("Pass: %d %d\n", result0, result1);
+        return(0);
+    }
+}
+
+int sub_vx_test_e16_m4(void)
+{  
+    int addend = 8;
+    int result0, result1, result2, result3;
+    asm volatile (
+        "li t1, 64 \n\t"
+        "vsetvli t0, t1, e16, m1 \n\t"
+        "vmv.v.i v4, 8 \n\t"
+        "vmv.v.i v5, 1 \n\t"
+        "vmv.v.i v6, 3 \n\t"
+        "vmv.v.i v7, 15 \n\t"
+        "vsetvli t0, t1, e16, m4\n\t"
+        "vsub.vx v8, v4, %4  \n\t"
+        "vsetvli t0, t1, e16, m1 \n\t"
+        "vmv.x.s %0, v8\n\t"
+        "vmv.x.s %1, v9\n\t"
+        "vmv.x.s %2, v10\n\t"
+        "vmv.x.s %3, v11\n\t"
+        : "=r" (result0), "=r" (result1), "=r" (result2), "=r" (result3)
+        : "r"  (addend)
+    );
+
+    if(result0 != 0 || result1 != -7 || result2 != -5 || result3 != 7)
+    {
+        printf("Fail: %d %d %d %d\n", result0, result1, result2, result3);
+        return(1);
+    }
+    else
+    {
+        printf("Pass: %d %d %d %d\n", result0, result1, result2, result3);
         return(0);
     }
 }
@@ -159,6 +485,70 @@ int sub_vx_test_e32_m1(void)
     }
 }
 
+int  sub_vx_test_e32_m2(void)
+{  
+    int subend = 8;
+    int result0, result1;
+    asm volatile (
+        "li t1, 64 \n\t"
+        "vsetvli t0, t1, e32, m1 \n\t"
+        "vmv.v.i v2, 3 \n\t"
+        "vmv.v.i v3, 12 \n\t"
+        "vsetvli t0, t1, e32, m2\n\t"
+        "vsub.vx v6, v2, %2 \n\t"
+        "vsetvli t0, t1, e32, m1 \n\t"
+        "vmv.x.s %0, v6\n\t"
+        "vmv.x.s %1, v7\n\t"
+        : "=r" (result0), "=r" (result1) 
+        : "r"  (subend)
+    );
+
+    if(result0 != -5 || result1 != 4)
+    {
+        printf("Fail: %d %d\n", result0, result1);
+        return(1);
+    }
+    else
+    {
+        printf("Pass: %d %d\n", result0, result1);
+        return(0);
+    }
+}
+
+int sub_vx_test_e32_m4(void)
+{  
+    int addend = 8;
+    int result0, result1, result2, result3;
+    asm volatile (
+        "li t1, 64 \n\t"
+        "vsetvli t0, t1, e32, m1 \n\t"
+        "vmv.v.i v4, 8 \n\t"
+        "vmv.v.i v5, 1 \n\t"
+        "vmv.v.i v6, 3 \n\t"
+        "vmv.v.i v7, 15 \n\t"
+        "vsetvli t0, t1, e32, m4\n\t"
+        "vsub.vx v8, v4, %4  \n\t"
+        "vsetvli t0, t1, e32, m1 \n\t"
+        "vmv.x.s %0, v8\n\t"
+        "vmv.x.s %1, v9\n\t"
+        "vmv.x.s %2, v10\n\t"
+        "vmv.x.s %3, v11\n\t"
+        : "=r" (result0), "=r" (result1), "=r" (result2), "=r" (result3)
+        : "r"  (addend)
+    );
+
+    if(result0 != 0 || result1 != -7 || result2 != -5 || result3 != 7)
+    {
+        printf("Fail: %d %d %d %d\n", result0, result1, result2, result3);
+        return(1);
+    }
+    else
+    {
+        printf("Pass: %d %d %d %d\n", result0, result1, result2, result3);
+        return(0);
+    }
+}
+
 
 
 
@@ -168,18 +558,33 @@ int main(void)
 
     printf("\nsub.vv\n");
     errors += sub_vv_test_e8_m1();
+    errors += sub_vv_test_e8_m2();
+    errors += sub_vv_test_e8_m4();
     errors += sub_vv_test_e16_m1();
+    errors += sub_vv_test_e16_m2();
+    errors += sub_vv_test_e16_m4();
     errors += sub_vv_test_e32_m1();
+    errors += sub_vv_test_e32_m2();
+    errors += sub_vv_test_e32_m4();
     printf("\nsub.vx\n");
     errors += sub_vx_test_e8_m1();
+    errors += sub_vx_test_e8_m2();
+    errors += sub_vx_test_e8_m4();
     errors += sub_vx_test_e16_m1();
+    errors += sub_vx_test_e16_m2();
+    errors += sub_vx_test_e16_m4();
     errors += sub_vx_test_e32_m1();
+    errors += sub_vx_test_e32_m2();
+    errors += sub_vx_test_e32_m4();
 
-
-    printf("\nErrors: %d\n", errors);
     
     if(errors > 0)
+    {
+        printf("\nErrors Detected!\nError Count: %d\n", errors);
         return(EXIT_FAILURE);
-    else
-        return(EXIT_SUCCESS);
+    }
+
+    printf("\nAll Tests Completed\nNo Errors Detected!\n");
+
+    return(EXIT_SUCCESS);
 }
