ae91c66c0ff4f796d5ec20a6691e8c262e2f35ed
mrj1g17 <mrj1g17@soton.ac.uk>
2020-12-18 19:47:59 +0000

    Fixed multiple inadequate/incorrect function names, added more variety to test cases

--- cv32/tests/programs/custom/vector_mul/vector_mul.c
+++ cv32/tests/programs/custom/vector_mul/vector_mul.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
-
-int smul_test_e8_m1(uint8_t vlen) 
+int vmul_vv_test_e8_m1(uint8_t vlen)
 {
     printf("Starting smul e8 m1 Test...");
     int8_t output;
     asm volatile (
-        "vsetvli t2, %1, e8\n\t"
+        "vsetvli t2, %1, e8, m1\n\t"
         "vmv.v.i v1, -5\n\t"
         "vmv.v.i v2, 5\n\t"
         "vmul.vv v1, v1, v2\n\t"
@@ -28,19 +26,19 @@ int smul_test_e8_m1(uint8_t vlen)
        printf("PASS\n");
        return(0);
     }
-    
+
 }
 
-int smul_test_e8_m2(uint8_t vlen) 
+int vmul_vv_test_e8_m2(uint8_t vlen)
 {
     printf("Starting smul e8 m2 Test...");
     int8_t output0, output1;
     asm volatile (
-        "vsetvli t2, %1, e8\n\t"
-        "vmv.v.i v2, -5\n\t"
-        "vmv.v.i v3, -5\n\t"
-        "vmv.v.i v4, 5\n\t"
-        "vmv.v.i v5, 5\n\t"
+        "vsetvli t2, %1, e8, m1\n\t"
+        "vmv.v.i v2, 6\n\t"
+        "vmv.v.i v3, -6\n\t"
+        "vmv.v.i v4, 6\n\t"
+        "vmv.v.i v5, 6\n\t"
         "vsetvli t2, %2, e8, m2\n\t"
         "vmul.vv v6, v2, v4\n\t"
         "vsetvli t2, %2, e8\n\t"
@@ -50,8 +48,8 @@ int smul_test_e8_m2(uint8_t vlen)
         : "r" (vlen)
     );
 
-    
-    if(output0 != -25 || output1 != -25)
+
+    if(output0 != 36 || output1 != -36)
     {
         printf("Test Complete! FAIL Output: v4: %d\tv5: %d\n", output0, output1);
        return(1);
@@ -61,19 +59,19 @@ int smul_test_e8_m2(uint8_t vlen)
          printf("Test Complete! PASS Output: v4: %d\tv5: %d\n", output0, output1);
        return(0);
     }
-    
+
 }
 
-int smul_test_e8_m4(uint8_t vlen) 
+int vmul_vv_test_e8_m4(uint8_t vlen)
 {
     printf("Starting smul e8 m4 Test...");
     int8_t output0, output1, output2, output3;
     asm volatile (
-        "vsetvli t2, %4, e8\n\t"
-        "vmv.v.i v4, -5\n\t"
+        "vsetvli t2, %4, e8, m1\n\t"
+        "vmv.v.i v4, -6\n\t"
         "vmv.v.i v5, -5\n\t"
-        "vmv.v.i v6, -5\n\t"
-        "vmv.v.i v7, -5\n\t"
+        "vmv.v.i v6, -4\n\t"
+        "vmv.v.i v7, -3\n\t"
         "vmv.v.i v8, 5\n\t"
         "vmv.v.i v9, 5\n\t"
         "vmv.v.i v10, 5\n\t"
@@ -90,7 +88,7 @@ int smul_test_e8_m4(uint8_t vlen)
     );
 
     printf("Test Complete! Output: v8: %d\tv9: %d\tv10: %d\tv11: %d\n", output0, output1, output2, output3);
-    if(output0 != -25 || output1 != -25 || output2 != -25 || output3 != -25)
+    if(output0 != -30 || output1 != -25 || output2 != -20 || output3 != -15)
     {
        printf("FAIL\n");
        return(1);
@@ -102,12 +100,12 @@ int smul_test_e8_m4(uint8_t vlen)
     }
 }
 
-int wmulv_test_e8_m1(uint8_t vlen) 
+int vwmul_vv_test_e8_m1(uint8_t vlen)
 {
     printf("Starting wmulv e8 m1 Test...");
     int16_t output;
     asm volatile (
-        "vsetvli t2, %1, e8\n\t"
+        "vsetvli t2, %1, e8, m1\n\t"
         "vmv.v.i v1, -16\n\t"
         "vmv.v.i v2, 14\n\t"
         "vsetvli t2, %1, e8, m1\n\t"
@@ -129,15 +127,15 @@ int wmulv_test_e8_m1(uint8_t vlen)
        printf("PASS\n");
        return(0);
     }
-    
+
 }
 
-int wmulv_test_e8_m2(uint8_t vlen) 
+int vwmul_vv_test_e8_m2(uint8_t vlen)
 {
     printf("Starting wmulv e8 m2 Test...");
     int16_t output0, output1;
     asm volatile (
-        "vsetvli t2, %2, e8\n\t"
+        "vsetvli t2, %2, e8, m1\n\t"
         "vmv.v.i v2, -16\n\t"
         "vmv.v.i v3, -16\n\t"
         "vmv.v.i v4, 14\n\t"
@@ -162,15 +160,15 @@ int wmulv_test_e8_m2(uint8_t vlen)
         printf("Test Complete! PASS Output: v4: %d\tv5: %d\n", output0, output1);
        return(0);
     }
-    
+
 }
 
-int wmulv_test_e8_m4(uint8_t vlen) 
+int vwmul_vv_test_e8_m4(uint8_t vlen)
 {
     printf("Starting wmulv e8 m4 Test...");
     int16_t output0, output1, output2, output3;
     asm volatile (
-        "vsetvli t2, %4, e8\n\t"
+        "vsetvli t2, %4, e8, m1\n\t"
         "vmv.v.i v4, -16\n\t"
         "vmv.v.i v5, -16\n\t"
         "vmv.v.i v6, -16\n\t"
@@ -203,15 +201,15 @@ int wmulv_test_e8_m4(uint8_t vlen)
     }
 }
 
-int wmulx_test_e8_m1(uint8_t vlen) 
+int vwmul_vx_test_e8_m1(uint8_t vlen)
 {
     printf("Starting wmul e8 m1 Test...");
     int16_t output;
     asm volatile (
         "li t1,-16\n\t"
-        "vsetvli t2, %1, e8\n\t"
+        "vsetvli t2, %1, e8, m1\n\t"
         "vmv.v.i v1, 10\n\t"
-        "vwmul.vx v2,v1, t1\n\t"
+        "vwmul.vx v2, v1, t1\n\t"
         "vsetvli t0, %1, e16,m1\n\t"
         "vmv.x.s %0, v2"
         : "=r" (output)
@@ -229,16 +227,16 @@ int wmulx_test_e8_m1(uint8_t vlen)
        printf("PASS\n");
        return(0);
     }
-    
+
 }
 
-int wmulx_test_e8_m2(uint8_t vlen) 
+int vwmul_vx_test_e8_m2(uint8_t vlen)
 {
     printf("Starting wmul e8 m2 Test...");
     int16_t output0, output1;
     asm volatile (
         "li t1,-16\n\t"
-        "vsetvli t2, %1, e8\n\t"
+        "vsetvli t2, %1, e8, m1\n\t"
         "vmv.v.i v2, 10\n\t"
         "vmv.v.i v3, 10\n\t"
         "vsetvli t2, %2, e8, m2\n\t"
@@ -250,7 +248,7 @@ int wmulx_test_e8_m2(uint8_t vlen)
         : "r" (vlen)
     );
 
- 
+
     if(output0 != -160 || output1 != -160)
     {
        printf("Test Complete! FAIL Output: v4: %d\tv5: %d\n", output0, output1);
@@ -261,17 +259,17 @@ int wmulx_test_e8_m2(uint8_t vlen)
         printf("Test Complete! PASS Output: v4: %d\tv5: %d\n", output0, output1);
        return(0);
     }
-    
+
 }
 
-int wmulx_test_e8_m4(uint8_t vlen) 
+int vwmul_vx_test_e8_m4(uint8_t vlen)
 {
     printf("Starting wmul e8 m4 Test...");
     int16_t output0, output1, output2, output3;
     uint8_t len;
     asm volatile (
         "li t1,-16\n\t"
-        "vsetvli t2, %4, e8\n\t"
+        "vsetvli t2, %4, e8, m1\n\t"
         "vmv.v.i v4, 10\n\t"
         "vmv.v.i v5, 10\n\t"
         "vmv.v.i v6, 10\n\t"
@@ -287,7 +285,7 @@ int wmulx_test_e8_m4(uint8_t vlen)
         : "r" (vlen)
     );
 
-    
+
     if(output0 != -160 || output1 != -160 || output2 != -160 || output3 != -160)
     {
         printf("FAIL\n");
@@ -305,12 +303,12 @@ int wmulx_test_e8_m4(uint8_t vlen)
 
 
 
-int smul_test_e16_m1(uint8_t vlen) 
+int vmul_vv_test_e16_m1(uint8_t vlen)
 {
     printf("Starting smul e16 m1 Test...");
     int16_t output;
-    asm volatile (   
-        "vsetvli t2, %1, e16\n\t"
+    asm volatile (
+        "vsetvli t2, %1, e16, m1\n\t"
         "vmv.v.i v1, -5\n\t"
         "vmv.v.i v2, 5\n\t"
         "vmul.vv v3, v1, v2\n\t"
@@ -330,19 +328,19 @@ int smul_test_e16_m1(uint8_t vlen)
        printf("PASS\n");
        return(0);
     }
-    
+
 }
 
-int smul_test_e16_m2(uint8_t vlen) 
+int vmul_vv_test_e16_m2(uint8_t vlen)
 {
     printf("Starting smul e16 m2 Test...");
     int16_t output0, output1;
     asm volatile (
-        "vsetvli t2, %2, e16\n\t"
+        "vsetvli t2, %2, e16, m1\n\t"
         "vmv.v.i v2, -5\n\t"
         "vmv.v.i v3, -5\n\t"
         "vmv.v.i v4, 5\n\t"
-        "vmv.v.i v5, 5\n\t"       
+        "vmv.v.i v5, 5\n\t"
         "vsetvli t2, %2, e16, m2\n\t"
         "vmul.vv v6, v2, v4\n\t"
         "vsetvli t2, %2, e16\n\t"
@@ -363,15 +361,15 @@ int smul_test_e16_m2(uint8_t vlen)
        printf("PASS\n");
        return(0);
     }
-    
+
 }
 
-int smul_test_e16_m4(uint8_t vlen) 
+int vmul_vv_test_e16_m4(uint8_t vlen)
 {
     printf("Starting smul e16 m4 Test...");
     int16_t output0, output1, output2, output3;
     asm volatile (
-        "vsetvli t2, %4, e16\n\t"
+        "vsetvli t2, %4, e16, m1\n\t"
         "vmv.v.i v4, -5\n\t"
         "vmv.v.i v5, -5\n\t"
         "vmv.v.i v6, -5\n\t"
@@ -406,12 +404,12 @@ int smul_test_e16_m4(uint8_t vlen)
 
 
 
-int wmulv_test_e16_m1(uint8_t vlen) 
+int vwmul_vv_test_e16_m1(uint8_t vlen)
 {
     printf("Starting wmulv e16 m1 Test...");
     int32_t output;
-    asm volatile (   
-        "vsetvli t2, %1, e16\n\t"
+    asm volatile (
+        "vsetvli t2, %1, e16, m1\n\t"
         "vmv.v.i v1, -16\n\t"
         "vmv.v.i v2, 14\n\t"
         "vsetvli t2, %1, e16, m1\n\t"
@@ -433,19 +431,19 @@ int wmulv_test_e16_m1(uint8_t vlen)
        printf("PASS\n");
        return(0);
     }
-    
+
 }
 
-int wmulv_test_e16_m2(uint8_t vlen) 
+int vwmul_vv_test_e16_m2(uint8_t vlen)
 {
     printf("Starting wmulv e16 m2 Test...");
     int32_t output0, output1;
     asm volatile (
-        "vsetvli t2, %2, e16\n\t"
+        "vsetvli t2, %2, e16, m1\n\t"
         "vmv.v.i v2, -16\n\t"
         "vmv.v.i v3, -16\n\t"
         "vmv.v.i v4, 14\n\t"
-        "vmv.v.i v5, 14\n\t"       
+        "vmv.v.i v5, 14\n\t"
         "vsetvli t2, %2, e16, m2\n\t"
         "vwmul.vv v8, v2, v4\n\t"
         "vsetvli t2, %2, e32\n\t"
@@ -466,15 +464,15 @@ int wmulv_test_e16_m2(uint8_t vlen)
        printf("PASS\n");
        return(0);
     }
-    
+
 }
 
-int wmulv_test_e16_m4(uint8_t vlen) 
+int vwmul_vv_test_e16_m4(uint8_t vlen)
 {
     printf("Starting wmulv e16 m4 Test...");
     int32_t output0, output1, output2, output3;
     asm volatile (
-        "vsetvli t2, %4, e16\n\t"
+        "vsetvli t2, %4, e16, m1\n\t"
         "vmv.v.i v4, -16\n\t"
         "vmv.v.i v5, -16\n\t"
         "vmv.v.i v6, -16\n\t"
@@ -508,16 +506,16 @@ int wmulv_test_e16_m4(uint8_t vlen)
 
 
 
-int wmulx_test_e16_m1(uint8_t vlen) 
+int vwmul_vx_test_e16_m1(uint8_t vlen)
 {
     printf("Starting wmul e16 m1 Test...");
     int32_t output;
     asm volatile (
         "li t1,-16\n\t"
-        "vsetvli t2, %1, e16\n\t"
+        "vsetvli t2, %1, e16, m1\n\t"
         "vmv.v.i v1, 10\n\t"
         "vwmul.vx v2, v1, t1\n\t"
-         "vsetvli t2, %1, e32\n\t"
+        "vsetvli t2, %1, e32\n\t"
         "vmv.x.s %0, v2"
         : "=r" (output)
         : "r" (vlen)
@@ -534,10 +532,10 @@ int wmulx_test_e16_m1(uint8_t vlen)
        printf("PASS\n");
        return(0);
     }
-    
+
 }
 
-int wmulx_test_e16_m2(uint8_t vlen) 
+int vwmul_vx_test_e16_m2(uint8_t vlen)
 {
     printf("Starting wmul e16 m2 Test...");
     int32_t output0, output1;
@@ -566,17 +564,17 @@ int wmulx_test_e16_m2(uint8_t vlen)
        printf("PASS\n");
        return(0);
     }
-    
+
 }
 
-int wmulx_test_e16_m4(uint8_t vlen) 
+int vwmul_vx_test_e16_m4(uint8_t vlen)
 {
     printf("Starting wmul e16 m4 Test...");
     int32_t output0, output1, output2, output3;
     uint8_t len;
     asm volatile (
         "li t1,-16\n\t"
-        "vsetvli t2, %4, e16\n\t"
+        "vsetvli t2, %4, e16, m1\n\t"
         "vmv.v.i v4, 10\n\t"
         "vmv.v.i v5, 10\n\t"
         "vmv.v.i v6, 10\n\t"
@@ -592,7 +590,7 @@ int wmulx_test_e16_m4(uint8_t vlen)
         : "r" (vlen)
     );
 
-    
+
     if(output0 != -160 || output1 !=  -160  || output2 !=  -160  || output3 !=  -160 )
     {
         printf("FAIL\n");
@@ -608,14 +606,14 @@ int wmulx_test_e16_m4(uint8_t vlen)
 }
 
 
-int smul_test_e32_m1(uint8_t vlen) 
+int vmul_vv_test_e32_m1(uint8_t vlen)
 {
     printf("Starting smul e32 m1 Test...");
     int32_t output;
     asm volatile (
-        "vsetvli t2, %1, e32\n\t"
+        "vsetvli t2, %1, e32, m1\n\t"
         "vmv.v.i v1, -5\n\t"
-        "vmv.v.i v2, 5\n\t"       
+        "vmv.v.i v2, 5\n\t"
         "vmul.vv v3, v1, v2\n\t"
         "vmv.x.s %0, v3"
         : "=r" (output)
@@ -633,20 +631,20 @@ int smul_test_e32_m1(uint8_t vlen)
        printf("PASS\n");
        return(0);
     }
-    
+
 }
 
-int smul_test_e32_m2(uint8_t vlen) 
+int vmul_vv_test_e32_m2(uint8_t vlen)
 {
     printf("Starting smul e32 m2 Test...");
     int32_t output0, output1;
     asm volatile (
 
-        "vsetvli t2, %2, e32\n\t"
+        "vsetvli t2, %2, e32, m1\n\t"
         "vmv.v.i v2, -5\n\t"
         "vmv.v.i v3, -5\n\t"
         "vmv.v.i v4, 5\n\t"
-        "vmv.v.i v5, 5\n\t"  
+        "vmv.v.i v5, 5\n\t"
         "vsetvli t2, %2, e32, m2\n\t"
         "vmul.vv v6, v2, v4\n\t"
         "vsetvli t2, %2, e32\n\t"
@@ -667,10 +665,10 @@ int smul_test_e32_m2(uint8_t vlen)
        printf("PASS\n");
        return(0);
     }
-    
+
 }
 
-int smul_test_e32_m4(uint8_t vlen) 
+int vmul_vv_test_e32_m4(uint8_t vlen)
 {
     printf("Starting smul e32 m4 Test...");
     int32_t output0, output1, output2, output3;
@@ -706,7 +704,7 @@ int smul_test_e32_m4(uint8_t vlen)
        printf("PASS\n");
        return(0);
     }
-      
+
 }
 
 
@@ -720,34 +718,34 @@ int main(int argc, char *argv[])
 
     int errors = 0;
 
-    errors += smul_test_e8_m1(64);
-    errors += smul_test_e8_m2(64);
-    errors += smul_test_e8_m4(64);
+    errors += vmul_vv_test_e8_m1(64);
+    errors += vmul_vv_test_e8_m2(64);
+    errors += vmul_vv_test_e8_m4(64);
     printf("\n");
-    errors += wmulx_test_e8_m1(64);
-    errors += wmulx_test_e8_m2(64);
-    errors += wmulx_test_e8_m4(64);
-    printf("\n");
-    errors += wmulv_test_e8_m1(64);
-    errors += wmulv_test_e8_m2(64);
-    errors += wmulv_test_e8_m4(64);
+    errors += vwmul_vv_test_e8_m1(64);
+    errors += vwmul_vv_test_e8_m2(64);
+    errors += vwmul_vv_test_e8_m4(64);
     printf("\n\n");
-    errors += smul_test_e16_m1(64);
-    errors += smul_test_e16_m2(64);
-    errors += smul_test_e16_m4(64);
+    errors += vwmul_vx_test_e8_m1(64);
+    errors += vwmul_vx_test_e8_m2(64);
+    errors += vwmul_vx_test_e8_m4(64);
     printf("\n");
-    errors += wmulv_test_e16_m1(64);
-    errors += wmulv_test_e16_m2(64);
-    errors += wmulv_test_e16_m4(64);
+    errors += vmul_vv_test_e16_m1(64);
+    errors += vmul_vv_test_e16_m2(64);
+    errors += vmul_vv_test_e16_m4(64);
     printf("\n");
-    errors += wmulx_test_e16_m1(64);
-    errors += wmulx_test_e16_m2(64);
-    errors += wmulx_test_e16_m4(64);
+    errors += vwmul_vv_test_e16_m1(64);
+    errors += vwmul_vv_test_e16_m2(64);
+    errors += vwmul_vv_test_e16_m4(64);
+    printf("\n");
+    errors += vwmul_vx_test_e16_m1(64);
+    errors += vwmul_vx_test_e16_m2(64);
+    errors += vwmul_vx_test_e16_m4(64);
     printf("\n\n");
-    errors += smul_test_e32_m1(64);
-    errors += smul_test_e32_m2(64);
-    errors += smul_test_e32_m4(64);
-  
+    errors += vmul_vv_test_e32_m1(64);
+    errors += vmul_vv_test_e32_m2(64);
+    errors += vmul_vv_test_e32_m4(64);
+
 
 
 
